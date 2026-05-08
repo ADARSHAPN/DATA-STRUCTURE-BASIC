f1067f20ad21d5d0b63694fa2abf6689564ac98f
ADARSHA P N <adarshnaik622@gmail.com>
2026-05-08 23:44:37 +0530

    Add files via upload

---
+++ LAB Program Ds.4.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#define MAX 5
+
+int stack[MAX],
+top = -1;
+
+//Push
+void push(int value) {
+    if (top == MAX-1){
+        printf("Stack overflow\n");
+        return;
+    }
+    else{
+        top++;
+        stack[top] = value;
+    printf("Pushed %d\n", value);
+}
+}
+//Pop   
+void pop() {
+    if (top == -1)
+        printf("Stack underflow\n");
+    
+    else{
+        printf("Popped %d\n", stack[top]);
+        top--;
+    }
+}
+//Display
+void display() {
+    if (top == -1)
+        printf("Stack is empty\n");
+    else {
+        printf("Stack elements: ");
+        for (int i = top; i >= 0; i--)
+            printf("%d ", stack[i]);
+        printf("\n");
+    }
+}
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+    push(60); // This will cause stack overflow
+
+    display();
+
+    pop();
+    pop();
+    pop();
+    pop();
+    pop();
+    pop(); // This will cause stack underflow
+
+    display();
+
+    return 0;
+}
